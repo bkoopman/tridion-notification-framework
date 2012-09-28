323cb225fc1b43aef2187a94a6a5a44718b3c942
Dominic Cronin <dominic.cronin@indivirtual.com>
2012-09-28 16:54:11 +0200

    Fixed up

--- TwitterNotifier.cs/TwitterNotifier.cs
+++ TwitterNotifier.cs/TwitterNotifier.cs
@@ -8,5 +8,24 @@ namespace TridionCommunity.NotificationFramework
 {
     public class TwitterNotifier: INotifier
     {
+//        function tweet([string] $status) {
+//   #http://blogs.msdn.com/shitals/archive/2008/12/27/9254245.aspx
+//  [System.Net.ServicePointManager]::Expect100Continue = $false
+//  try {
+//    $wc = new-object System.Net.WebClient
+//    $wc.BaseAddress = "http://twitter.com"
+//    $wc.Credentials = new-object System.Net.NetworkCredential
+//    $wc.Credentials.UserName = "Your account name"
+//    $wc.Credentials.Password = "password"
+//    $stream = $wc.OpenWrite("statuses/update.xml")
+//    $writer = new-object System.IO.StreamWriter -ArgumentList $stream
+//    $writer.Write("status=" + $status) 
+//  }
+//  finally {
+//    $writer.Dispose()
+//    $stream.Dispose()
+//    $wc.Dispose()
+//  }
+//}
     }
 }
