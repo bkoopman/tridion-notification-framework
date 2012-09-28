e6417381989ed50568341ed35d965c9f00daa133
Dominic Cronin <dominic.cronin@indivirtual.com>
2012-09-28 16:43:03 +0200

    Fixed up namespaces and added Added TwitterNotifier.cs
    Signed-off-by: Dominic Cronin <dominic.cronin@indivirtual.com>

--- EmailNotifier/EmailNotifier.cs
+++ EmailNotifier/EmailNotifier.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Net.Mail;
 
-namespace EmailNotifier
+namespace TridionCommunity.NotificationFramework
 {
     public class EmailNotifier : INotifier
     {
--- Tridion.Notification.Framework.Contracts/INotifier.cs
+++ Tridion.Notification.Framework.Contracts/INotifier.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace EmailNotifier
+namespace TridionCommunity.NotificationFramework
 {
     public interface INotifier
     {
