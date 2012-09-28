c0e48d5b398de1e3d143d9038f7f379e087e2c99
Andrey Marchuk <marchuk.andrew@gmail.com>
2012-09-28 19:52:34 +0200

    Added some code

--- NotificationService/NotificationService/CoreService/Client.cs
+++ NotificationService/NotificationService/CoreService/Client.cs
@@ -1,15 +1,59 @@
-﻿using Tridion.ContentManager.CoreService.Client;
+﻿using System;
+using System.Configuration;
+using System.ServiceModel;
+using Tridion.ContentManager.CoreService.Client;
 
 
 namespace NotificationService.CoreService
 {
     public class Client
     {
-        public static SessionAwareCoreServiceClient GetCoreService(string username)
+        public static SessionAwareCoreServiceClient GetCoreService()
         {
-            var result = new SessionAwareCoreServiceClient();
-            result.Impersonate(username);
+            var result = new SessionAwareCoreServiceClient("wsHttp_2011");
+
+
+            result.Impersonate(ConfigurationManager.AppSettings.Get("adminUser"));
             return result;
         }
+
+        public static EndpointAddress SetCorrectServer(EndpointAddress currentAddress)
+        {
+            var newHost = new Uri(ConfigurationManager.AppSettings.Get("server"));
+
+            if (currentAddress.Uri.Scheme == "net.tcp")
+            {
+                currentAddress = new EndpointAddress(currentAddress.ToString().Replace(currentAddress.Uri.Host, newHost.Host));
+            }
+            else
+            {
+                currentAddress = new EndpointAddress(currentAddress.ToString().Replace(currentAddress.Uri.Authority, newHost.Authority));
+            }
+
+            return currentAddress;
+        }
+
+        public static T GetNewClient<T>()
+        {
+            object obj;
+                        
+            if (typeof(T) == typeof(SessionAwareCoreServiceClient))
+            {
+                SessionAwareCoreServiceClient client = new SessionAwareCoreServiceClient(ConfigurationManager.AppSettings.Get("endpointName"));
+                client.ChannelFactory.Endpoint.Address = SetCorrectServer(client.ChannelFactory.Endpoint.Address);
+                obj = client;
+            }
+
+            else
+            {
+                CoreServiceClient client = new CoreServiceClient(ConfigurationManager.AppSettings.Get("endpointName"));
+                client.ChannelFactory.Endpoint.Address = SetCorrectServer(client.ChannelFactory.Endpoint.Address);
+                obj = client;
+            }
+
+            return (T)obj;
+        }
+
+
     }
 }
--- NotificationService/NotificationService/Worker.cs
+++ NotificationService/NotificationService/Worker.cs
@@ -11,49 +11,49 @@ namespace NotificationService
     {
         internal static void DoWork()
         {
-            //// get a corservice client
-            //var client = Client.GetCoreService("CURRENT USER");
+            // get a corservice client
+            var client = Client.GetCoreService();
+            client.GetCurrentUser();
+            var users = client.GetSystemWideList(new UsersFilterData { BaseColumns = ListBaseColumns.IdAndTitle, IsPredefined = false });
+            var userIds = users.Select(f => f.Id).Distinct().ToArray();
+            var applicationDatas = client.ReadApplicationDataForSubjectsIds(userIds, new[] { "test" }).Where(a => a.Value.Length > 0);
 
-            //var users = client.GetSystemWideList(new UsersFilterData { BaseColumns = ListBaseColumns.IdAndTitle, IsPredefined = false });
-            //var userIds = users.Select(f => f.Id).Distinct().ToArray();
-            //var applicationDatas = client.ReadApplicationDataForSubjectsIds(userIds, new[] { "test" }).Where(a => a.Value.Length > 0);
-
-            //foreach (var applicationDataElement in applicationDatas)
-            //{
-            //    if (true)
-            //    {
-            //        client.Impersonate(users.Single(u => u.Id == applicationDataElement.Key).Title);
-
-
-            //        // get the workflow items for the user
-            //        UserWorkItemsFilterData userWorkItemsFilter = new UserWorkItemsFilterData();
-            //        userWorkItemsFilter.ActivityState = ActivityState.Started | ActivityState.Assigned;
-
-            //        // get assignemnt and work list
-            //        IdentifiableObjectData[] workFlowItems = client.GetSystemWideList(userWorkItemsFilter);
-
-            //        // Read
-            //        DateTime fakeDate = DateTime.Now;
-
-            //        IList<WorkItemData> relevantWorkFlowDataItems = new List<WorkItemData>();
-
-            //        // then if there is a taks
-            //        foreach (WorkItemData workItem in workFlowItems)
-            //        {
-            //            if (fakeDate < workItem.VersionInfo.CreationDate)
-            //            {
-            //                relevantWorkFlowDataItems.Add(workItem);
-            //                // add to list of things to push
-            //                // hand that list, appdata, user ide, user desc & name to the notifier
-            //            }
-
-            //        }
-
-            //        // push these buggers :)
+            foreach (var applicationDataElement in applicationDatas)
+            {
+                if (true)
+                {
+                    client.Impersonate(users.Single(u => u.Id == applicationDataElement.Key).Title);
 
 
-            //    }
-            //}
+                    // get the workflow items for the user
+                    UserWorkItemsFilterData userWorkItemsFilter = new UserWorkItemsFilterData();
+                    userWorkItemsFilter.ActivityState = ActivityState.Started | ActivityState.Assigned;
+
+                    // get assignemnt and work list
+                    IdentifiableObjectData[] workFlowItems = client.GetSystemWideList(userWorkItemsFilter);
+
+                    // Read
+                    DateTime fakeDate = DateTime.Now;
+
+                    IList<WorkItemData> relevantWorkFlowDataItems = new List<WorkItemData>();
+
+                    // then if there is a taks
+                    foreach (WorkItemData workItem in workFlowItems)
+                    {
+                        if (fakeDate < workItem.VersionInfo.CreationDate)
+                        {
+                            relevantWorkFlowDataItems.Add(workItem);
+                            // add to list of things to push
+                            // hand that list, appdata, user ide, user desc & name to the notifier
+                        }
+
+                    }
+
+                    // push these buggers :)
+
+
+                }
+            }
         }
     }
 }
