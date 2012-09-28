e8ac529ffb1954966b7a7d4628349ea13ecc07ba
Andrey Marchuk <marchuk.andrew@gmail.com>
2012-09-28 18:28:10 +0200

    Updated service

--- NotificationService/NotificationService/WorkflowNotificationService.Designer.cs
+++ NotificationService/NotificationService/WorkflowNotificationService.Designer.cs
@@ -2,59 +2,14 @@
 using NotificationService.CoreService;
 using Tridion.ContentManager.CoreService.Client;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace NotificationService
 {
     partial class WorkflowNotificationService : ServiceBase
     {
 
-        protected override void OnStart(string[] args)
-        {
-            // get a corservice client
-            var client = Client.GetCoreService("CURRENT USER");
-
-            var users = client.GetSystemWideList(new UsersFilterData { BaseColumns = ListBaseColumns.IdAndTitle, IsPredefined = false });
-            var userIds = users.Select(f => f.Id).Distinct().ToArray();
-            var applicationDatas = client.ReadApplicationDataForSubjectsIds(userIds, new[] { "test" }).Where(a => a.Value.Length > 0);
-
-            foreach (var applicationDataElement in applicationDatas)
-            {
-                if (true)
-                {
-                    client.Impersonate(users.Single(u => u.Id == applicationDataElement.Key).Title);
-
-
-                    // get the workflow items for the user
-                    UserWorkItemsFilterData userWorkItemsFilter = new UserWorkItemsFilterData();
-                    userWorkItemsFilter.ActivityState = ActivityState.Started | ActivityState.Assigned;
-
-                    // get assignemnt and work list
-                    IdentifiableObjectData[] workFlowItems = client.GetSystemWideList(userWorkItemsFilter);
-
-                    // Read
-                    DateTime fakeDate = DateTime.Now;
-
-                    IList<WorkItemData> relevantWorkFlowDataItems = new List<WorkItemData>();
-
-                    // then if there is a taks
-                    foreach (WorkItemData workItem in workFlowItems)
-                    {
-                        if (fakeDate < workItem.VersionInfo.CreationDate)
-                        {
-                            relevantWorkFlowDataItems.Add(workItem);
-                            // add to list of things to push
-                            // hand that list, appdata, user ide, user desc & name to the notifier
-                        }
-
-                    }
-
-                    // push these buggers :)
-
-
-                }
-            }
-        }
-
+        
         /// <summary> 
         /// Required designer variable.
         /// </summary>
@@ -81,8 +36,11 @@ namespace NotificationService
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            // 
+            // WorkflowNotificationService
+            // 
             this.ServiceName = "WorkflowNotificationService";
+
         }
 
         #endregion
