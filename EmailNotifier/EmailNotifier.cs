ff82fb4242fb356f376efe7ddde4bd6dd1ac2946
Albert <s990330@gmail.com>
2012-09-28 15:58:26 +0200

    F

---
+++ EmailNotifier/EmailNotifier.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Mail;
+
+namespace EmailNotifier
+{
+    public class EmailNotifier : INotifier
+    {
+
+        public EmailNotifier()
+        {
+            SendMail();
+        }
+
+        private void SendMail()
+        {
+            using (MailMessage mail = new MailMessage("workflowmailer@mycompany.com", "user@address.com"))
+            {
+                mail.Subject = "Your notifications";
+                mail.Body = "Stuff waiting for you";
+
+                using (SmtpClient smtp = new SmtpClient())
+                {
+                    smtp.Send(mail);
+                }
+            }
+        }
+       
+       
+    
+
+    }
+}
---
+++ Tridion.Notification.Framework.Contracts/INotificationMessage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tridion.Notification.Framework.Contracts
+{
+    public class INotificationMessage
+    {
+        public string AppData { get; }
+    }
+}
