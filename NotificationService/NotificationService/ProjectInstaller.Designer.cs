23153d25beca4cb5f898dec05ea1a8a3561f88f1
Andrey Marchuk <marchuk.andrew@gmail.com>
2012-09-29 11:48:38 +0200

    Updated service

--- NotificationService/NotificationService/ProjectInstaller.Designer.cs
+++ NotificationService/NotificationService/ProjectInstaller.Designer.cs
@@ -39,7 +39,9 @@
             // 
             // serviceInstaller1
             // 
-            this.serviceInstaller1.ServiceName = "WorkflowNotificationService";
+            this.serviceInstaller1.Description = "Workflow Notification Service";
+            this.serviceInstaller1.ServiceName = "Workflow Notification Service";
+            this.serviceInstaller1.StartType = System.ServiceProcess.ServiceStartMode.Automatic;
             // 
             // ProjectInstaller
             // 
