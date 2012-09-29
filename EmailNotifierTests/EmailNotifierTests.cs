d611afa212151a56d8a832fd7a8019bc7ae10fa0
Albert <s990330@gmail.com>
2012-09-29 11:47:42 +0200

    tests

---
+++ EmailNotifierTests/EmailNotifierTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EmailNotifierTests
+{
+    [TestClass]
+    public class EmailNotifierTests
+    {
+        [TestMethod]
+        public void TransformInputToMailBodySuccess()
+        {
+            
+        }
+
+    }
+}
